b59f67abdca1409857e67cb01d8fd71af03dc9bd
Raunchers <maethridge@gmail.com>
2024-01-02 23:57:39 -0500

    Added a spreadsheet for the output of the calculations

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 export default function Home(){
 
@@ -13,9 +13,6 @@ export default function Home(){
 
   // State to hold previously calculated scenarios. An array of obj
   const [allPredicts, setAllPredicts] = useState([]);
-
-  // State to store all form bodies with their calculations
-  const [allFormBodies, setAllFormBodies] = useState([]);
 
   // Function to handle storing user inputted info in the variables
   const handleInputChange = (e) => {
@@ -42,16 +39,20 @@ export default function Home(){
   };
 
   const handleSubmit = () => {
+    // User inputted percent of asset to sell
+    let userPercent = (percentAmnt/100);
     // Starting sell price
     let currentPrice = startPrice;
     // Remaining amount of asset, starts at the total asset amount the user entered
     let remainingAsset = totalAmnt;
     // Starting asset amount to be sold. Will be updated in the while loop
-    let sellAmnt = totalAmnt * percentAmnt;
+    let sellAmnt = totalAmnt * userPercent;
     // Running total of gross profits
     let grossProfits = 0;
-    // Index variable for the newly added calculations
-    const index = allPredicts.length;
+
+    if(totalAmnt <= 0){
+      return;
+    }
 
     // run loop to calculate total profit
     while(currentPrice <= endPrice){
@@ -62,102 +63,31 @@ export default function Home(){
       remainingAsset = remainingAsset - sellAmnt;
 
       // Calculate new sell amount
-      sellAmnt = remainingAsset * percentAmnt;
+      sellAmnt = remainingAsset * userPercent;
 
       // Increment priceChange
       currentPrice += ratePriceChange;
-    }
 
-    // Obj for results
-    const results = {
-      totalAmnt: totalAmnt, // Starting total asset amount
-      remainingAmnt: remainingAsset.toFixed(2), // Remaining asset amount
-      percentAmnt: (percentAmnt * 100).toFixed(2), // percent of asset to be sold at each price level
-      ratePriceChange: ratePriceChange.toFixed(2),
-      startPrice: startPrice.toFixed(2), // Starting price level
-      endPrice: endPrice.toFixed(2), // Ending price level
-      grossProfit: grossProfits.toFixed(2), // Gross profits
-    };
+      console.log("Sell Amnt: ", sellAmnt);
+      console.log("Sell Price: ", currentPrice);
+      console.log("Remaining Balance: ", remainingAsset);
+      console.log("Gross Profit Per Round: ", grossProfits);
+    }
 
     // Update state array to store calculated results
     setAllPredicts((prevCalc) => [...prevCalc, {
       totalAmnt: totalAmnt, // Starting total asset amount
-      remainingAmnt: remainingAsset, // Remaining asset amount
+      remainingAmnt: remainingAsset.toFixed(2), // Remaining asset amount
       percentAmnt: percentAmnt, // percent of asset to be sold at each price level
       ratePriceChange: ratePriceChange.toFixed(2),
-      startPrice: startPrice, // Starting price level
-      endPrice: endPrice, // Ending price level
-      grossProfit: grossProfits, // Gross profits
+      startPrice: startPrice.toFixed(2), // Starting price level
+      endPrice: endPrice.toFixed(2), // Ending price level
+      grossProfit: grossProfits.toFixed(2), // Gross profits
     }]);
-
-    // Create a new form body with the current calculation
-    const newFormBody = (
-        <div className="profitOutputBody" key={index}>
-          <ul>
-            <li>
-              <strong>Starting Asset Balance:</strong>
-              <input type="text" value={`$${results.totalAmnt}`} disabled />
-            </li>
-            <li>
-              <strong>Remaining Asset Balance:</strong>
-              <input type="text" value={`${results.remainingAmnt}`} disabled />
-            </li>
-            <li>
-              <strong>Percent of asset to be sold:</strong>
-              <input type="text" value={`${results.percentAmnt}%`} disabled />
-            </li>
-            <li>
-              <strong>Price Increment:</strong>
-              <input type="text" value={`$${results.ratePriceChange}`} disabled />
-            </li>
-            <li>
-              <strong>Starting Sell Price:</strong>
-              <input type="text" value={`$${results.startPrice}`} disabled />
-            </li>
-            <li>
-              <strong>Ending Sell Price:</strong>
-              <input type="text" value={`$${results.endPrice}`} disabled />
-            </li>
-            <li>
-              <strong>Gross Profits:</strong>
-              <input type="text" value={`$${results.grossProfit}`} disabled />
-            </li>
-          </ul>
-        </div>
-    );
-
-    // Update the use state
-    setAllFormBodies((prevFormBodies) => [...prevFormBodies, newFormBody]);
-
-    // Reset inputs to placeholder values
-    handleResetValues();
   };
 
-  // Reset back to the default values for the next calculation. Add previous calculation to the array.
-  const handleResetValues = () => {
-
-    // Get the input IDs
-    let inputTotal = document.getElementById("totalAmnt");
-    let inputPercent = document.getElementById("percentAmnt");
-    let inputPriceChange = document.getElementById("priceChange");
-    let inputStartP = document.getElementById("startPrice");
-    let inputEndP = document.getElementById("endPrice");
-
-    // reset the input fields and show their defaults
-    inputTotal.value = "";
-    inputTotal.placeholder = "Total Asset Amount";
-
-    inputPercent.value = "";
-    inputPercent.placeholder = "Percent of asset to be sold";
-
-    inputPriceChange.value = "";
-    inputPriceChange.placeholder = "Price increment";
-
-    inputStartP.value = "";
-    inputStartP.placeholder = "Price to start selling";
-
-    inputEndP.value = "";
-    inputEndP.placeholder = "Ending sell price";
+  const handleReloadButtonClick = () => {
+    window.location.reload();
   };
 
   return(
@@ -205,17 +135,50 @@ export default function Home(){
               <button type="submit" onClick={handleSubmit}>
                 Submit
               </button>
+              <button type="submit" onClick={handleReloadButtonClick}>
+                Clear Page
+              </button>
             </div>
           </div> {/* End of form */}
           <div className="spacer"></div>
         </div>
-        <div className="formContainer">
-          {/* Display calculated information */}
-          <div className="formBodiesContainer">
-            {allFormBodies.map((formBody, index) => (
-                <React.Fragment key={index}>{formBody}</React.Fragment>
-            ))}
+        <div className="container">
+          <div className="spacer"></div>
+          <h1>Results Spreadsheet</h1>
+          <div className="spacer"></div>
+        </div>
+        <div className="container">
+          <div className="spacer"></div>
+          <div className="spreadsheet">
+            {/* Display calculated information in a spreadsheet */}
+            <table>
+              <thead>
+              <tr>
+                <th>Starting Asset Balance</th>
+                <th>Remaining Asset Balance</th>
+                <th>Percent To Sell</th>
+                <th>Price Increment</th>
+                <th>Starting Sell Price</th>
+                <th>Ending Sale Price</th>
+                <th>Gross Profits</th>
+              </tr>
+              </thead>
+              <tbody>
+              {allPredicts.map((result, index) => (
+                  <tr key={index}>
+                    <td>{result.totalAmnt}</td>
+                    <td>{result.remainingAmnt}</td>
+                    <td>%{result.percentAmnt}</td>
+                    <td>${result.ratePriceChange}</td>
+                    <td>${result.startPrice}</td>
+                    <td>${result.endPrice}</td>
+                    <td>${result.grossProfit}</td>
+                  </tr>
+              ))}
+              </tbody>
+            </table>
           </div>
+          <div className="spacer"></div>
         </div>
       </div>
   );
